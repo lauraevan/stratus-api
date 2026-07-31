2bb6d7ed52ffff1392dd062d3b56bfbc4f789bb2
Polaris <laurajeanbixler@gmail.com>
2026-07-31 17:57:54 -0500

    Fix require path for createAccount function

--- api/api.js
+++ api/api.js
@@ -732,7 +732,7 @@ app.post("/cloud/v1/createSession", auth, async (req, res) => {
 
   try {
     push({ status: "creating_account" });
-    const acc = await require("./mail-providers").createAccount();
+    const acc = await require("./mail-providers 4.js").createAccount();
 
     releaseAccountSlot(apiKey);
 
