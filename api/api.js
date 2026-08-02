40cd3052b39097cea62aaca2d2659f432980c7d9
Polaris <264532369+lauraevan@users.noreply.github.com>
2026-08-02 16:19:10 -0500

    Update embed endpoint to use new HTML file

--- api/api.js
+++ api/api.js
@@ -634,7 +634,7 @@ app.get("/cloud/v1/embed", (req, res) => {
   if (!req.query.id) {
     return res.status(400).type("text").send("Missing `id` parameter");
   }
-res.sendFile(path.join(__dirname, "public", "e-team-ember.html"));
+res.sendFile(path.join(__dirname, "public", "e-team-ember-v6.html"));
 });
 
 app.get("/cloud/v1/embed-data", (req, res) => {
